d3bf985a6da0e7f1f82d64ce68825af036df5f62
Scott Bromander <scott@primeacademy.io>
2017-03-14 16:35:37 -0500

    update port for heroku

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
-app.set("port", 5000);
+app.set("port", (process.env.PORT || 5000));
 
 app.get("/*", function(req,res){
     console.log(req.params);
